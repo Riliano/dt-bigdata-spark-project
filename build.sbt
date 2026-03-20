1144194323ee96444a5304b8dddff9be923b4986
Lazar Nenovski <lazar.nenovski@gmail.com>
2026-03-20 20:22:22 +0000

    initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "cardDWH"
+version := "0.1"
+scalaVersion := "2.13.16"
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "4.0.1")
+
---
+++ src/main/scala/CardDWH.scala
@@ -0,0 +1,10 @@
+/* Apache Spark */
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+
+
+object cardDWH {
+  def main(args: Array[String]) {
+    println("Hello Scala!")
+  }
+}
